af69442f0dd587fcc67ae458e573d24a2feb32ca
Celia Amador <celdrakes@gmail.com>
2024-02-12 12:39:48 +0100

    Fix prettier command to include subdirectories (#19)
    * Fix prettier command to include subdirectories
    
    * Correcting Prettier and Eslint config
    
    * Fix files after config updates

--- src/app/components/Fleet/FleetList.tsx
+++ src/app/components/Fleet/FleetList.tsx
@@ -15,7 +15,7 @@ import {
   Title,
   Toolbar,
   ToolbarContent,
-  ToolbarItem
+  ToolbarItem,
 } from '@patternfly/react-core';
 import { ActionsColumn, Table, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
 import { FleetList } from '@types';
@@ -41,52 +41,52 @@ const FleetTable = () => {
   if (!fleetList?.items?.length) {
     // TODO UXD needed (design, copy and placing)
     return (
-        <EmptyState>
-          <EmptyStateHeader titleText={<>You haven&apos;t created any fleets yet</>} headingLevel="h4" />
-          <EmptyStateBody>Create a new fleet using the &quot;Create&quot; button</EmptyStateBody>
-        </EmptyState>
-    )
+      <EmptyState>
+        <EmptyStateHeader titleText={<>You haven&apos;t created any fleets yet</>} headingLevel="h4" />
+        <EmptyStateBody>Create a new fleet using the &quot;Create&quot; button</EmptyStateBody>
+      </EmptyState>
+    );
   }
 
   return (
-      <Table aria-label="Fleets table">
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th>Source</Th>
-            <Th>Devices</Th>
-            <Th>In-sync</Th>
-            <Th>Status</Th>
-            <Td />
-          </Tr>
-        </Thead>
-        <Tbody>
-          {fleetList.items.map((fleet) => (
-            <Tr key={fleet.metadata.name}>
-              <Td dataLabel="Name">
-                <Link to={`${fleet.metadata.name}`}>{fleet.metadata.name}</Link>
-              </Td>
-              <Td dataLabel="Source">{fleet.spec.template.spec.os?.image || '-'}</Td>
-              <Td dataLabel="Devices">-</Td>
-              <Td dataLabel="In-sync">-</Td>
-              <Td dataLabel="Status">-</Td>
-              <Td isActionCell>
-                <ActionsColumn
-                  items={[
-                    {
-                      title: 'Delete',
-                      onClick: async () => {
-                        await remove(`fleets/${fleet.metadata.name}`);
-                        refetch();
-                      },
+    <Table aria-label="Fleets table">
+      <Thead>
+        <Tr>
+          <Th>Name</Th>
+          <Th>Source</Th>
+          <Th>Devices</Th>
+          <Th>In-sync</Th>
+          <Th>Status</Th>
+          <Td />
+        </Tr>
+      </Thead>
+      <Tbody>
+        {fleetList.items.map((fleet) => (
+          <Tr key={fleet.metadata.name}>
+            <Td dataLabel="Name">
+              <Link to={`${fleet.metadata.name}`}>{fleet.metadata.name}</Link>
+            </Td>
+            <Td dataLabel="Source">{fleet.spec.template.spec.os?.image || '-'}</Td>
+            <Td dataLabel="Devices">-</Td>
+            <Td dataLabel="In-sync">-</Td>
+            <Td dataLabel="Status">-</Td>
+            <Td isActionCell>
+              <ActionsColumn
+                items={[
+                  {
+                    title: 'Delete',
+                    onClick: async () => {
+                      await remove(`fleets/${fleet.metadata.name}`);
+                      refetch();
                     },
-                  ]}
-                />
-              </Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
+                  },
+                ]}
+              />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
   );
 };
 
