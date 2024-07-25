be29bb6614a63bf2a218b1a42fabd8262d66bdf7
Celia Amador <camadorg@redhat.com>
2024-07-25 11:57:25 +0200

    EDM-244 Change status when SpecValid error triggers

--- libs/ui-components/src/components/Status/SystemUpdateStatus.tsx
+++ libs/ui-components/src/components/Status/SystemUpdateStatus.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 
-import { ConditionStatus, ConditionType, DeviceStatus, DeviceUpdatedStatusType } from '@flightctl/types';
+import { ConditionStatus, ConditionType, DeviceStatus } from '@flightctl/types';
 import { useTranslation } from '../../hooks/useTranslation';
-import StatusDisplay from './StatusDisplay';
+import StatusDisplay, { StatusDisplayContent } from './StatusDisplay';
 import { getSystemUpdateStatusItems } from '../../utils/status/system';
-import { getConditionMessage } from '../../utils/error';
 
 const SystemUpdateStatus = ({ deviceStatus }: { deviceStatus?: DeviceStatus }) => {
   const { t } = useTranslation();
@@ -15,18 +14,15 @@ const SystemUpdateStatus = ({ deviceStatus }: { deviceStatus?: DeviceStatus }) =
     (c) => c.type === ConditionType.DeviceSpecValid && c.status === ConditionStatus.ConditionStatusFalse,
   );
 
-  let computedStatus = deviceStatus?.updated.status;
-  let message = deviceStatus?.updated.info;
-
   if (invalidSpec) {
-    computedStatus = DeviceUpdatedStatusType.DeviceUpdatedStatusOutOfDate;
-    message = getConditionMessage(invalidSpec, t('Invalid device spec'));
+    const title = t('Invalid configuration');
+    return <StatusDisplayContent level="danger" label={title} messageTitle={title} message={invalidSpec.message} />;
   }
 
   const item = statusItems.find((statusItem) => {
-    return statusItem.id === computedStatus;
+    return statusItem.id === deviceStatus?.updated.status;
   });
-  return <StatusDisplay item={item} message={message} />;
+  return <StatusDisplay item={item} message={deviceStatus?.updated.info} />;
 };
 
 export default SystemUpdateStatus;
