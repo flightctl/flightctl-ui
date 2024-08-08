497b8f12c15cb234ca172936f2ad59458645a20f
Celia Amador <camadorg@redhat.com>
2024-08-08 10:52:19 +0200

    Use OverviewPage that includes page title

---
+++ apps/ocp-plugin/src/components/Overview/OverviewPage.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Overview from '@flightctl/ui-components/src/components/OverviewPage/OverviewPage';
+
+const OverviewPage = () => {
+  return <Overview />;
+};
+
+export default OverviewPage;
