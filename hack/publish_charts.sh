b64f81a06dff9485836d2e731d6e998eb0696bdd
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2024-08-16 11:21:50 +0200

    EDM-296: Fix helm chart publish script

--- hack/publish_charts.sh
+++ hack/publish_charts.sh
@@ -10,5 +10,5 @@ helm package ./apps/standalone/deploy/helm/flightctl-ui --version "${VERSION}" -
 helm package ./apps/ocp-plugin/deploy/helm/flightctl-ocp-ui --version "${VERSION}" --app-version "${VERSION}"
 
 #login with helm registry login quay.io -u ${USER} -p ${PASSWORD}
-helm push "flightctl-ui-${VERSION}.tgz" oci://${{ env.QUAY_CHARTS }}/
-helm push "flightctl-ocp-ui-${VERSION}.tgz" oci://${{ env.QUAY_CHARTS }}/
+helm push "flightctl-ui-${VERSION}.tgz" "oci://${QUAY_CHARTS}/"
+helm push "flightctl-ocp-ui-${VERSION}.tgz" "oci://${QUAY_CHARTS}/"
