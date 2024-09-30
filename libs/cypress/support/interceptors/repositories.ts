7254d88ce5d0b655ce5533aeba28c14b70ade0e0
Celia Amador <camadorg@redhat.com>
2024-09-30 12:30:33 +0200

    EDM-527: Intercept get repository details request to avoid 401

--- libs/cypress/support/interceptors/repositories.ts
+++ libs/cypress/support/interceptors/repositories.ts
@@ -14,6 +14,18 @@ const loadInterceptors = () => {
       body: buildRepositoriesResponse(repoList),
     });
   }).as('repository-list');
+
+  cy.intercept('GET', '/api/flightctl/api/v1/repositories/**', (req) => {
+    const repoName = req.url.match(/\/repositories\/(.+)$/)[1];
+    const found = repoList.find((repo) => repo.metadata.name === repoName);
+    if (found) {
+      req.reply({
+        body: repoList.find((repo) => repo.metadata.name === repoName),
+      });
+    } else {
+      req.reply(404);
+    }
+  }).as('get-repo-details');
 };
 
 export { loadInterceptors };
