c3b09923f6d719dc83fa2abb74777294b6e8dc7d
Celia Amador <camadorg@redhat.com>
2024-10-11 13:15:44 +0200

    EDM-593: Initial UI adaptations to new filtering and sorting API mechanisms

--- libs/ui-components/src/utils/sort/generic.ts
+++ libs/ui-components/src/utils/sort/generic.ts
@@ -1,4 +1,4 @@
-import { Device, EnrollmentRequest, ObjectMeta } from '@flightctl/types';
+import { Device, ObjectMeta } from '@flightctl/types';
 
 export const sortByName = <R extends { metadata: ObjectMeta }>(resources: R[]) =>
   resources.sort((a, b) => {
@@ -19,13 +19,6 @@ export const sortByLastSeenDate = (devices: Device[]) =>
 
     const aDate = getDate(a);
     const bDate = getDate(b);
-    return new Date(aDate).getTime() - new Date(bDate).getTime();
-  });
-
-export const sortByCreationDate = (enrollments: EnrollmentRequest[]) =>
-  enrollments.sort((a, b) => {
-    const aDate = a.metadata.creationTimestamp || 0;
-    const bDate = b.metadata.creationTimestamp || 0;
     return new Date(aDate).getTime() - new Date(bDate).getTime();
   });
 
