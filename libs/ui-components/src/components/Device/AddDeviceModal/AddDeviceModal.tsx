68de4099ff082c71db6837bc83a8123a6d04bc30
Celia Amador <camadorg@redhat.com>
2025-05-08 13:05:52 +0200

    EDM-1548: Update Patternfly modals to next version compatible with PF6

--- libs/ui-components/src/components/Device/AddDeviceModal/AddDeviceModal.tsx
+++ libs/ui-components/src/components/Device/AddDeviceModal/AddDeviceModal.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Button, List, ListComponent, ListItem, Modal, OrderType, Stack, StackItem } from '@patternfly/react-core';
+import { Button, List, ListComponent, ListItem, OrderType, Stack, StackItem } from '@patternfly/react-core';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from '@patternfly/react-core/next';
 
 import { useTranslation } from '../../../hooks/useTranslation';
 import { ADDING_NEW_DEVICES_LINK } from '../../../links';
@@ -9,26 +10,29 @@ const AddDeviceModal = ({ onClose }: { onClose: VoidFunction }) => {
   const { t } = useTranslation();
 
   return (
-    <Modal variant="small" title={t('Add devices')} onClose={onClose} isOpen>
-      <Stack hasGutter>
-        <StackItem>{t('You can add devices following these steps:')}</StackItem>
-        <StackItem>
-          <List component={ListComponent.ol} type={OrderType.number}>
-            <ListItem>{t('Request an enrollment certificate for your device')}</ListItem>
-            <ListItem>{t('Build a bootc OS image')}</ListItem>
-            <ListItem>{t('Create, sign and publish a bootable OS disk image')}</ListItem>
-            <ListItem>{t('Boot your device into the OS disk image')}</ListItem>
-          </List>
-        </StackItem>
-        <StackItem>
-          <LearnMoreLink link={ADDING_NEW_DEVICES_LINK} text={t('Learn more about adding devices')} />
-        </StackItem>
-        <StackItem className="pf-v5-u-mt-md">
-          <Button variant="primary" isInline onClick={onClose}>
-            {t('Close')}
-          </Button>
-        </StackItem>
-      </Stack>
+    <Modal variant="small" onClose={onClose} isOpen>
+      <ModalHeader title={t('Add devices')} />
+      <ModalBody>
+        <Stack hasGutter>
+          <StackItem>{t('You can add devices following these steps:')}</StackItem>
+          <StackItem>
+            <List component={ListComponent.ol} type={OrderType.number}>
+              <ListItem>{t('Request an enrollment certificate for your device')}</ListItem>
+              <ListItem>{t('Build a bootc OS image')}</ListItem>
+              <ListItem>{t('Create, sign and publish a bootable OS disk image')}</ListItem>
+              <ListItem>{t('Boot your device into the OS disk image')}</ListItem>
+            </List>
+          </StackItem>
+          <StackItem>
+            <LearnMoreLink link={ADDING_NEW_DEVICES_LINK} text={t('Learn more about adding devices')} />
+          </StackItem>
+        </Stack>
+      </ModalBody>
+      <ModalFooter>
+        <Button variant="primary" isInline onClick={onClose}>
+          {t('Close')}
+        </Button>
+      </ModalFooter>
     </Modal>
   );
 };
