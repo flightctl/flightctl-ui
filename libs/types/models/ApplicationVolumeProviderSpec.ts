67a862c14263d38cae64516df223e9a535f644b8
Celia Amador <camadorg@redhat.com>
2025-06-03 09:24:54 +0200

    EDM-1497: Update API types for Application volumes

---
+++ libs/types/models/ApplicationVolumeProviderSpec.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { ApplicationVolume } from './ApplicationVolume';
+export type ApplicationVolumeProviderSpec = {
+  /**
+   * List of application volumes.
+   */
+  volumes?: Array<ApplicationVolume>;
+};
+
