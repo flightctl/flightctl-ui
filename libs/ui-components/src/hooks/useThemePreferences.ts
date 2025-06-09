87a18916deaec0dc5459d3f3fccae7dde498b2ba
Celia Amador Gonzalez <camadorg@redhat.com>
2025-06-09 15:10:20 +0200

    EDM-1634: Switch YAML viewer theme (#304)

--- libs/ui-components/src/hooks/useThemePreferences.ts
+++ libs/ui-components/src/hooks/useThemePreferences.ts
@@ -2,18 +2,12 @@ import * as React from 'react';
 import { useUserPreferences } from './useUserPreferences';
 
 export const THEME_LOCAL_STORAGE_KEY = 'flightctl/theme';
+export const OCP_CONSOLE_THEME_LOCAL_STORAGE_KEY = 'bridge/theme';
+
 const THEME_DARK_CLASS = 'pf-v5-theme-dark';
 
 export type Theme = 'dark' | 'light' | 'system';
-
-export const updateThemeClass = (htmlTagElement: HTMLElement, theme: string | null) => {
-  const systemTheme: Theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  if (theme === 'dark' || (!theme && systemTheme === 'dark')) {
-    htmlTagElement.classList.add(THEME_DARK_CLASS);
-  } else {
-    htmlTagElement.classList.remove(THEME_DARK_CLASS);
-  }
-};
+export type ResolvedTheme = Exclude<Theme, 'system'>;
 
 const getTheme = (storageTheme: string | null): Theme => {
   switch (storageTheme) {
@@ -29,9 +23,26 @@ const getTheme = (storageTheme: string | null): Theme => {
   }
 };
 
+const getDarkThemeMq = () => window.matchMedia('(prefers-color-scheme: dark)');
+
+const getResolvedTheme = (darkThemeMq: MediaQueryList, theme: string | null): ResolvedTheme => {
+  const isDarkPreferred = darkThemeMq.matches;
+  return theme === 'dark' || (isDarkPreferred && getTheme(theme) === 'system') ? 'dark' : 'light';
+};
+
+export const updateThemeClass = (htmlTagElement: HTMLElement, resolvedTheme: ResolvedTheme) => {
+  if (resolvedTheme === 'dark') {
+    htmlTagElement.classList.add(THEME_DARK_CLASS);
+  } else {
+    htmlTagElement.classList.remove(THEME_DARK_CLASS);
+  }
+};
+
 export const useThemePreferences = () => {
   const htmlTagElement = document.documentElement;
-  const [value, setValue] = useUserPreferences(THEME_LOCAL_STORAGE_KEY);
+  const [userTheme, setUserTheme] = useUserPreferences(THEME_LOCAL_STORAGE_KEY);
+  const [consoleTheme] = useUserPreferences(OCP_CONSOLE_THEME_LOCAL_STORAGE_KEY);
+  const [resolvedTheme, setResolvedTheme] = React.useState<ResolvedTheme>('light');
 
   React.useEffect(() => {
     const mqListener = (e: MediaQueryListEvent) => {
@@ -41,24 +52,40 @@ export const useThemePreferences = () => {
         htmlTagElement.classList.remove(THEME_DARK_CLASS);
       }
     };
-    const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
-    if (!value) {
-      darkThemeMq.addEventListener('change', mqListener);
+    const darkThemeMq = getDarkThemeMq();
+    let actualTheme: ResolvedTheme;
+    if (consoleTheme) {
+      actualTheme = getResolvedTheme(darkThemeMq, consoleTheme);
+    } else {
+      actualTheme = getResolvedTheme(darkThemeMq, userTheme);
+      updateThemeClass(htmlTagElement, actualTheme);
+      if (!userTheme) {
+        darkThemeMq.addEventListener('change', mqListener);
+      }
     }
-    updateThemeClass(htmlTagElement, value);
-    return () => darkThemeMq.removeEventListener('change', mqListener);
-  }, [htmlTagElement, value]);
+    setResolvedTheme(actualTheme);
+
+    return () => {
+      if (!consoleTheme && !userTheme) {
+        darkThemeMq.removeEventListener('change', mqListener);
+      }
+    };
+  }, [htmlTagElement, userTheme, consoleTheme]);
 
   const setThemeState = React.useCallback(
     (theme: Theme) => {
-      setValue(theme);
-      updateThemeClass(htmlTagElement, theme);
+      const darkTheme = getDarkThemeMq();
+      const actualTheme = getResolvedTheme(darkTheme, theme);
+      updateThemeClass(htmlTagElement, actualTheme);
+      setUserTheme(theme);
+      setResolvedTheme(actualTheme);
     },
-    [htmlTagElement, setValue],
+    [htmlTagElement, setUserTheme],
   );
 
   return {
-    theme: getTheme(value),
-    setTheme: setThemeState,
+    userTheme: getTheme(userTheme),
+    setUserTheme: setThemeState,
+    resolvedTheme,
   };
 };
