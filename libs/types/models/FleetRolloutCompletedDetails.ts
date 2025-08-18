e9c9536f9e3acb8fd83c67fb6eb8e461ea1bc9ae
Celia Amador <camadorg@redhat.com>
2025-08-18 12:41:39 +0200

    Update API types to Aug 18th

---
+++ libs/types/models/FleetRolloutCompletedDetails.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type FleetRolloutCompletedDetails = {
+  /**
+   * The type of detail for discriminator purposes.
+   */
+  detailType: 'FleetRolloutCompleted';
+  /**
+   * The name of the TemplateVersion that this fleet rollout is completed for.
+   */
+  templateVersion: string;
+};
+
---
+++ libs/types/models/FleetRolloutDeviceSelectedDetails.ts
@@ -0,0 +1,19 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type FleetRolloutDeviceSelectedDetails = {
+  /**
+   * The type of detail for discriminator purposes.
+   */
+  detailType: 'FleetRolloutDeviceSelected';
+  /**
+   * The name of the fleet that the device is being selected for.
+   */
+  fleetName: string;
+  /**
+   * The name of the TemplateVersion that the device is being selected to render.
+   */
+  templateVersion: string;
+};
+
