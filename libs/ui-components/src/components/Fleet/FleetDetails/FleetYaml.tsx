c59f1d6c33f95f6e53683ebef405c68bb69c4b28
Celia Amador <camadorg@redhat.com>
2025-10-03 12:25:26 +0200

    EDM-2249: The yaml tab is now a YAML editor similar to the OpenShift console

---
+++ libs/ui-components/src/components/Fleet/FleetDetails/FleetYaml.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Fleet } from '@flightctl/types';
+
+import YamlEditor from '../../common/CodeEditor/YamlEditor';
+
+// In the YAML editor, we must have the raw Fleet object, without extra details that are not part of the PATCH response.
+// For that reason, we must remove the extra "devicesSummary" field that was added to the fleet details.
+const FleetYaml = ({ fleet, refetch }: { fleet: Fleet; refetch: VoidFunction }) => {
+  const fleetWithoutSummary = { ...fleet, status: { ...fleet.status, devicesSummary: undefined } } as Fleet;
+
+  return <YamlEditor apiObj={fleetWithoutSummary} refetch={refetch} />;
+};
+
+export default FleetYaml;
