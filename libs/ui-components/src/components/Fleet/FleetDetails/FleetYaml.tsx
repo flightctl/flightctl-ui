39675a719523923f1ae4a8598d7d07883c27021e
Celia Amador <camadorg@redhat.com>
2025-10-09 14:45:29 +0200

    Handle empty case in YamlEditor

--- libs/ui-components/src/components/Fleet/FleetDetails/FleetYaml.tsx
+++ libs/ui-components/src/components/Fleet/FleetDetails/FleetYaml.tsx
@@ -7,7 +7,6 @@ import YamlEditor from '../../common/CodeEditor/YamlEditor';
 // For that reason, we must remove the extra "devicesSummary" field that was added to the fleet details.
 const FleetYaml = ({ fleet, refetch }: { fleet: Fleet; refetch: VoidFunction }) => {
   const fleetWithoutSummary = { ...fleet, status: { ...fleet.status, devicesSummary: undefined } } as Fleet;
-
   return <YamlEditor apiObj={fleetWithoutSummary} refetch={refetch} />;
 };
 
