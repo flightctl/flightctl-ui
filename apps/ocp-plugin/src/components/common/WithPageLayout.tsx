5cfa6ab9d6f622a68604d5b2711b7b15195da028
Celia Amador <camadorg@redhat.com>
2025-11-14 09:05:50 +0100

    Fix applications structure to prevent API requests in LoginPage

--- apps/ocp-plugin/src/components/common/WithPageLayout.tsx
+++ apps/ocp-plugin/src/components/common/WithPageLayout.tsx
@@ -1,27 +1,13 @@
 import * as React from 'react';
 import OrganizationGuard from '@flightctl/ui-components/src/components/common/OrganizationGuard';
-
-// Restore WithPageLayoutContent when organizations are enabled for OCP plugin
-// The context is still needed since "useOrganizationGuardContext" is used in common components
-/*
-const WithPageLayoutContent = ({ children }: React.PropsWithChildren) => {
-  const { isOrganizationSelectionRequired } = useOrganizationGuardContext();
-
-  return isOrganizationSelectionRequired ? (
-    <OrganizationSelector isFirstLogin />
-  ) : (
-    <>
-      <PageNavigation />
-      {children}
-    </>
-  );
-};
-*/
+import { SystemRestoreProvider } from '@flightctl/ui-components/src/hooks/useSystemRestoreContext';
 
 const WithPageLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <OrganizationGuard>
-      <>{children}</>
+      <SystemRestoreProvider>
+        <>{children}</>
+      </SystemRestoreProvider>
     </OrganizationGuard>
   );
 };
