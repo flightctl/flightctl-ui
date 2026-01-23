f80ee13f9bc583710f97ba34bacef4b81b48b489
Celia Amador <camadorg@redhat.com>
2026-01-23 12:06:05 +0100

    Remove Ansible lib

--- libs/ansible/src/components/ScrollablePage/ScrollablePage.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import './ScrollablePage.css';
-
-const ScrollablePage = ({ children }: React.PropsWithChildren) => {
-  return <div className="fctl-scrollable-page">{children}</div>;
-};
-
-export default ScrollablePage;
