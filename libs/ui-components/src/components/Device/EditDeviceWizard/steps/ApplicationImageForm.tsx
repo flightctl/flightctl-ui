bbf3f51fa561bee368eb754407aa8510ed75b873
Celia Amador <camadorg@redhat.com>
2026-01-27 18:03:10 +0100

    Add app privilege to Quadlet applications too

--- libs/ui-components/src/components/Device/EditDeviceWizard/steps/ApplicationImageForm.tsx
+++ libs/ui-components/src/components/Device/EditDeviceWizard/steps/ApplicationImageForm.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
+import { useField } from 'formik';
+import { Content, FormGroup, FormSection, Grid, Switch } from '@patternfly/react-core';
 
 import { FormGroupWithHelperText } from '../../../common/WithHelperText';
 import TextField from '../../../form/TextField';
 import LearnMoreLink from '../../../common/LearnMoreLink';
+import { DefaultHelperText } from '../../../form/FieldHelperText';
 import { useTranslation } from '../../../../hooks/useTranslation';
 import { useAppLinks } from '../../../../hooks/useAppLinks';
-import { ComposeImageAppForm, QuadletImageAppForm } from '../../../../types/deviceSpec';
+import {
+  ComposeImageAppForm,
+  QuadletImageAppForm,
+  RUN_AS_DEFAULT_USER,
+  RUN_AS_ROOT_USER,
+  isQuadletImageAppForm,
+} from '../../../../types/deviceSpec';
 
 const ApplicationImageForm = ({
   app,
@@ -18,24 +27,73 @@ const ApplicationImageForm = ({
 }) => {
   const { t } = useTranslation();
   const createAppLink = useAppLinks('createApp');
+  const appFieldName = `applications[${index}]`;
+  const isQuadlet = isQuadletImageAppForm(app);
+  const [{ value: runAs }, , { setValue: setRunAs }] = useField<string | undefined>(`${appFieldName}.runAs`);
+  const isRootless = runAs !== RUN_AS_ROOT_USER;
 
   return (
-    <FormGroupWithHelperText
-      label={t('Image')}
-      content={
-        <span>
-          {t('The application image. Learn how to create one')} <LearnMoreLink text={t('here')} link={createAppLink} />
-        </span>
-      }
-      isRequired
-    >
-      <TextField
-        aria-label={t('Image')}
-        name={`applications.${index}.image`}
-        value={app.image || ''}
-        isDisabled={isReadOnly}
-      />
-    </FormGroupWithHelperText>
+    <Grid hasGutter>
+      <FormGroupWithHelperText
+        label={t('Image')}
+        content={
+          <span>
+            {t('The application image. Learn how to create one')}{' '}
+            <LearnMoreLink text={t('here')} link={createAppLink} />
+          </span>
+        }
+        isRequired
+      >
+        <TextField
+          aria-label={t('Image')}
+          name={`applications.${index}.image`}
+          value={app.image || ''}
+          isDisabled={isReadOnly}
+        />
+      </FormGroupWithHelperText>
+      {isQuadlet && (
+        <FormSection title={t('Access & permissions')}>
+          <FormGroup>
+            <Switch
+              id={`${appFieldName}-system-integrity-switch`}
+              aria-label={t('System integrity protection')}
+              label={isRootless ? t('System integrity protection enabled') : t('System integrity protection disabled')}
+              isChecked={isRootless}
+              onChange={async (_, checked) => {
+                await setRunAs(checked ? RUN_AS_DEFAULT_USER : RUN_AS_ROOT_USER);
+              }}
+              isDisabled={isReadOnly}
+            />
+            <DefaultHelperText
+              helperText={
+                <Content component="small">
+                  {t(
+                    'Prevents this workload from modifying critical host operating system files. We recommend keeping this enabled to maintain system integrity.',
+                  )}
+                </Content>
+              }
+            />
+          </FormGroup>
+          {isRootless && (
+            <FormGroup label={t('Rootless user identity')} isRequired>
+              <TextField
+                aria-label={t('Rootless user identity')}
+                name={`${appFieldName}.runAs`}
+                value={runAs || RUN_AS_DEFAULT_USER}
+                isDisabled
+                readOnly
+                helperText={t(
+                  "By default, workloads run as the '{{ runAsUser }}' user. To specify a custom user identity, edit the application configuration via YAML or CLI.",
+                  {
+                    runAsUser: RUN_AS_DEFAULT_USER,
+                  },
+                )}
+              />
+            </FormGroup>
+          )}
+        </FormSection>
+      )}
+    </Grid>
   );
 };
 
--- libs/ui-components/src/components/Device/EditDeviceWizard/steps/ApplicationInlineForm.tsx
+++ libs/ui-components/src/components/Device/EditDeviceWizard/steps/ApplicationInlineForm.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { FieldArray } from 'formik';
-import { Button, FormGroup, Grid, Split, SplitItem } from '@patternfly/react-core';
+import { FieldArray, useField } from 'formik';
+import { Button, Content, FormGroup, FormSection, Grid, Split, SplitItem, Switch } from '@patternfly/react-core';
 import MinusCircleIcon from '@patternfly/react-icons/dist/js/icons/minus-circle-icon';
 import PlusCircleIcon from '@patternfly/react-icons/dist/js/icons/plus-circle-icon';
 
@@ -9,7 +9,14 @@ import CheckboxField from '../../../form/CheckboxField';
 import UploadField from '../../../form/UploadField';
 import TextField from '../../../form/TextField';
 import ExpandableFormSection from '../../../form/ExpandableFormSection';
-import { ComposeInlineAppForm, QuadletInlineAppForm } from '../../../../types/deviceSpec';
+import { DefaultHelperText } from '../../../form/FieldHelperText';
+import {
+  ComposeInlineAppForm,
+  QuadletInlineAppForm,
+  RUN_AS_DEFAULT_USER,
+  RUN_AS_ROOT_USER,
+  isQuadletInlineAppForm,
+} from '../../../../types/deviceSpec';
 
 const MAX_INLINE_FILE_SIZE_BYTES = 1024 * 1024;
 
@@ -64,63 +71,111 @@ const ApplicationInlineForm = ({
   isReadOnly?: boolean;
 }) => {
   const { t } = useTranslation();
+  const appFieldName = `applications[${index}]`;
+  const isQuadlet = isQuadletInlineAppForm(app);
+  const [{ value: runAs }, , { setValue: setRunAs }] = useField<string | undefined>(`${appFieldName}.runAs`);
+  const isRootless = runAs !== RUN_AS_ROOT_USER;
 
   if (isReadOnly && !app.files?.length) {
     return null;
   }
 
   return (
-    <FieldArray name={`applications.${index}.files`}>
-      {({ push, remove }) => (
-        <>
-          {app.files?.map((file, fileIndex) => {
-            const fieldName = `applications[${index}].files[${fileIndex}]`;
-            return (
-              <Split key={fileIndex} hasGutter>
-                <SplitItem isFilled>
-                  <InlineApplicationFileForm
-                    file={file}
-                    fileIndex={fileIndex}
-                    fileFieldName={fieldName}
-                    isReadOnly={isReadOnly}
-                  />
-                </SplitItem>
-                {!isReadOnly && app.files.length > 1 && (
-                  <SplitItem>
-                    <Button
-                      aria-label={t('Delete file')}
-                      variant="link"
-                      icon={<MinusCircleIcon />}
-                      iconPosition="start"
-                      onClick={() => remove(fileIndex)}
+    <Grid hasGutter>
+      <FieldArray name={`applications.${index}.files`}>
+        {({ push, remove }) => (
+          <>
+            {app.files?.map((file, fileIndex) => {
+              const fieldName = `applications[${index}].files[${fileIndex}]`;
+              return (
+                <Split key={fileIndex} hasGutter>
+                  <SplitItem isFilled>
+                    <InlineApplicationFileForm
+                      file={file}
+                      fileIndex={fileIndex}
+                      fileFieldName={fieldName}
+                      isReadOnly={isReadOnly}
                     />
                   </SplitItem>
-                )}
-              </Split>
-            );
-          })}
+                  {!isReadOnly && app.files.length > 1 && (
+                    <SplitItem>
+                      <Button
+                        aria-label={t('Delete file')}
+                        variant="link"
+                        icon={<MinusCircleIcon />}
+                        iconPosition="start"
+                        onClick={() => remove(fileIndex)}
+                      />
+                    </SplitItem>
+                  )}
+                </Split>
+              );
+            })}
 
-          {!isReadOnly && (
-            <FormGroup>
-              <Button
-                variant="link"
-                icon={<PlusCircleIcon />}
-                iconPosition="start"
-                onClick={() => {
-                  push({
-                    path: '',
-                    content: '',
-                    base64: false,
-                  });
-                }}
-              >
-                {t('Add file')}
-              </Button>
+            {!isReadOnly && (
+              <FormGroup>
+                <Button
+                  variant="link"
+                  icon={<PlusCircleIcon />}
+                  iconPosition="start"
+                  onClick={() => {
+                    push({
+                      path: '',
+                      content: '',
+                      base64: false,
+                    });
+                  }}
+                >
+                  {t('Add file')}
+                </Button>
+              </FormGroup>
+            )}
+          </>
+        )}
+      </FieldArray>
+      {isQuadlet && (
+        <FormSection title={t('Access & permissions')}>
+          <FormGroup>
+            <Switch
+              id={`${appFieldName}-system-integrity-switch`}
+              aria-label={t('System integrity protection')}
+              label={isRootless ? t('System integrity protection enabled') : t('System integrity protection disabled')}
+              isChecked={isRootless}
+              onChange={async (_, checked) => {
+                await setRunAs(checked ? RUN_AS_DEFAULT_USER : RUN_AS_ROOT_USER);
+              }}
+              isDisabled={isReadOnly}
+            />
+            <DefaultHelperText
+              helperText={
+                <Content component="small">
+                  {t(
+                    'Prevents this workload from modifying critical host operating system files. We recommend keeping this enabled to maintain system integrity.',
+                  )}
+                </Content>
+              }
+            />
+          </FormGroup>
+          {isRootless && (
+            <FormGroup label={t('Rootless user identity')} isRequired>
+              <TextField
+                aria-label={t('Rootless user identity')}
+                name={`${appFieldName}.runAs`}
+                value={runAs || RUN_AS_DEFAULT_USER}
+                isDisabled
+                readOnly
+                helperText={t(
+                  "By default, workloads run as the '{{ runAsUser }}' user. To specify a custom user identity, edit the application configuration via YAML or CLI.",
+                  {
+                    runAsUser: RUN_AS_DEFAULT_USER,
+                  },
+                )}
+              />
             </FormGroup>
           )}
-        </>
+        </FormSection>
       )}
-    </FieldArray>
+    </Grid>
   );
 };
 
