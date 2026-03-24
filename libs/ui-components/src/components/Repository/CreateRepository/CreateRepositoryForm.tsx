d253ba1b1572daa4b0e43b9f0adc90cb44304ca1
Rastislav Wagner <rawagner@redhat.com>
2026-03-24 10:52:55 +0100

    EDM-3470: Update RS description (#587)

--- libs/ui-components/src/components/Repository/CreateRepository/CreateRepositoryForm.tsx
+++ libs/ui-components/src/components/Repository/CreateRepository/CreateRepositoryForm.tsx
@@ -427,7 +427,7 @@ const CreateRepositoryFormContent = ({ isEdit, onClose, options, children }: Cre
                 <LabelWithHelperText
                   label={t('Use resource syncs')}
                   content={t(
-                    'A resource sync is an automated Gitops method that helps manage your imported fleets by monitoring source repository changes and updating your fleet configuration accordingly.',
+                    "A resource sync is an automated Gitops method that helps manage your imported fleets or catalogs by monitoring source repository changes and updating resource's configuration accordingly.",
                   )}
                 />
               }
