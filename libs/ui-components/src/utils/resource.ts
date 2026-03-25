958ddbef112da3d367d53fd5ea1124e16bd6303a
Celia Amador Gonzalez <camadorg@redhat.com>
2026-03-25 13:18:45 +0100

    EDM-3580: Fix error 404 in device catalog for managed devices (#594)

--- libs/ui-components/src/utils/resource.ts
+++ libs/ui-components/src/utils/resource.ts
@@ -1,4 +1,16 @@
-import { ObjectMeta } from '@flightctl/types';
+import { ObjectMeta, ResourceKind } from '@flightctl/types';
+
+export const getOwnerName = (ownerKind: ResourceKind, owner: string | undefined): string | undefined => {
+  if (!owner) {
+    return undefined;
+  }
+  const prefix = `${ownerKind}/`;
+  if (!owner.startsWith(prefix)) {
+    return undefined;
+  }
+  const name = owner.slice(prefix.length);
+  return name.length > 0 ? name : undefined;
+};
 
 export const getResourceId = <R extends { kind: string; metadata: ObjectMeta }>(resource: R) =>
   `${resource.kind}/${resource.metadata.name || ''}`;
